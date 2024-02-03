befa6da851dd006f487a47303140098e890f4d0b
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-02-03 19:09:49 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 function App() {
     return (
         <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
+            <Title>hello world</Title>
         </div>
     );
 }
